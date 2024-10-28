49de8c7278485c8ed6d5e2df6037c5ce53466988
tando1 <tanya.do1@students.mq.edu.au>
2024-10-28 14:43:33 +1100

    intro, hover imgs

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
   $('.muscle-diagram img').hover(function() {
-      $('#description').text('Targeted Workout: Squats');
+      $(this).siblings('.description').text('Targeted Workout: ' + ($(this).attr('alt') === 'Squat Diagram' ? 'Barbell or Dumbbell Squats' : 'Bench Press'));
   }, function() {
-      $('#description').text('Hover over a muscle to see workouts');
-  });
+      $(this).siblings('.description').text('Hover over a muscle to see workout to do');
+  });  
 
   $('.accordion h3').click(function() {
       $(this).next('.content').slideToggle();
